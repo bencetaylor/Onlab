403247613a4013f9460e4feac3a86513fe2da51e
DESKTOP-NJV7KVA\Bence <bencetaylor@gmail.com>
2018-03-06 12:27:46 +0100

    Added user roles - NOT working

--- CarRent/CarRent/Data/DbInitializer.cs
+++ CarRent/CarRent/Data/DbInitializer.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CarRent.Models.CarRentModels;
+using CarRent.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace CarRent.Data
 {
@@ -11,6 +14,49 @@ namespace CarRent.Data
         public static void Initialize(ApplicationDbContext context)
         {
             context.Database.EnsureCreated();
+
+            /*
+            var UserManager = new UserManager<ApplicationUser>(context);
+            var roleManager = new RoleManager<IdentityRole<int>>();
+            //new UserStore<ApplicationUser>(context)
+            // In Startup iam creating first Admin Role and creating a default Admin User     
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool    
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                   
+
+                var user = new ApplicationUser();
+                user.UserName = "Admin";
+                user.Email = "admin@gmail.com";
+
+                string userPWD = "Ab-1234";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin    
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+            // creating Creating Manager role     
+            if (!roleManager.RoleExists("Regular"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Regular";
+                roleManager.Create(role);
+
+            }
+            */
+
+            
 
             if (context.Cars.Any())
             {
